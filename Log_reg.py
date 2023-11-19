f85b442f0e5477c637decdff6219e808bb20816d
Mental1107 <mslee020807@gmail.com>
2023-11-19 21:14:51 +0900

    fix log_reg

--- Log_reg.py
+++ Log_reg.py
@@ -1,20 +1,56 @@
 import numpy as np
 import pandas as pd
-df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',header=None)
+import json
 from sklearn.model_selection import train_test_split
-x, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, stratify=y, random_state=0)
+from embed_and_norm import *
+x=[]
+y=[]
+x_test=[]
+y_test=[]
+with open('dataset/03_04.json') as file:
+    stat = json.load(file)
+with open('dataset/03_04_score.json') as file:
+    score = json.load(file)
+x.extend(stat)
+y.extend(score)
 
+with open('dataset/04_05.json') as file:
+    stat = json.load(file)
+with open('dataset/04_05_score.json') as file:
+    score = json.load(file)
+x.extend(stat)
+y.extend(score)
 
-def one_hot_encode_multiclass(y_change, num_classes=None):
-    if num_classes is None:
-        num_classes = np.max(y_change) + 1
+with open('dataset/05_06.json') as file:
+    stat = json.load(file)
+with open('dataset/05_06_score.json') as file:
+    score = json.load(file)
+x.extend(stat)
+y.extend(score)
+with open('dataset/06_07.json') as file:
+    stat = json.load(file)
+with open('dataset/06_07_score.json') as file:
+    score = json.load(file)
+x.extend(stat)
+y.extend(score)
 
-    one_hot_labels = np.eye(num_classes)[y_change]
+x_train = x
+x_train = np.array(x_train)
+y_train = y
+y_train = [(int(pair[0]),int(pair[1])) for pair in y_train]
+y_train = [list(pair) for pair in y_train]
+y_train = score_to_target(y_train)
+#X_TEST/Y_TEST
+with open('dataset/07_08.json') as file:
+    stat = json.load(file)
+with open('dataset/07_08_score.json') as file:
+    score = json.load(file)
+x_test.extend(stat)
+y_test.extend(score)
+y_copy = y_test
+x_test = np.array(x_test)
+y_test = score_to_target(y_test)
 
-    return one_hot_labels
-y_train = one_hot_encode_multiclass(y_train)
-y_test = one_hot_encode_multiclass(y_test)
 class LogisticRegression:
     def __init__(self, x_train, y_train, x_test, y_test):
         self._x_train = x_train
@@ -26,54 +62,61 @@ class LogisticRegression:
         size_y = self._y_train.shape
         size_y = int(size_y[1])
         self.bias = np.zeros((size_x, size_y))
+
     def sigmoid(self, x, bias):
         return 1.0 / (1.0 + np.exp(-np.dot(x, bias)))
+
     def cost(self, x, y, bias):
         size = self._x_train.shape
         size = int(size[0])
-        delta = 1e-6  # 무한대 방지
+        delta = 1e-10  # 무한대 방지
         h = self.sigmoid(x, bias)
         costs = y * np.log(h + delta) + (1 - y) * np.log(1 - h + delta)
-        return (-1 / size) * (np.sum(costs, axis=0))  # 각 행의 합 계산후 평균값을 구해 cost계산
+        return (-1 / size) * (np.sum(costs, axis=0))
+
     def learn(self, lr, epoch):
-        costs=[]
-        size = self._x_train.shape
-        size = int(size[0])
+        costs = []
+        size = self._x_train.shape[0]
+        jlen = self._y_train.shape[1]
         for i in range(epoch):
             # 10개의 숫자별로 계산
-            for j in range(4):
-                dt = (self.sigmoid(self._x_train, self.bias[:, j]) - self._y_train[:, j]).reshape(size,1)* self._x_train
+            for j in range(jlen):
+                dt = (self.sigmoid(self._x_train, self.bias[:, j]) - self._y_train[:, j]).reshape(size,
+                                                                                                  1) * self._x_train
                 # 그래디언트 백터 계산
                 self.bias[:, j] = self.bias[:, j] - lr * np.sum(dt, axis=0)
-                # Learning Rate를 곱한 후 bias에 적용
+
             cost = self.cost(self._x_train, self._y_train, self.bias)
-            print(i, 'epoch, cost: ',cost)
+            print(i, 'epoch, cost: ', cost)
             costs.append(cost)
-        return self.bias,costs
+        return self.bias, costs
+
     def predict(self, x, bias):
         size = self.bias.shape
         size = int(size[0])
         prediction = self.sigmoid(x.reshape(1, size), bias)
         return np.argmax(prediction)
 
-#학습
-log = LogisticRegression(x_train, y_train, x_test, y_test)
-bias, costs = log.learn(0.000001, 1500)
-#정확도 예측 초기화
-acc = 0
-suc_rate=[0 for i in range(4)]
-app_count=[0 for i in range(4)]
-#실제 정확도 계산/출력
-x, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, stratify=y, random_state=0)
-for i in range(0, 54):
-    print(i, ' predict: ', log.predict(x_test[i], bias), ' real: ', y_test[i])
-    app_count[y_test[i]]+=1
-    if log.predict(x_test[i], bias) == y_test[i]:
-        acc += 1.0
-        suc_rate[y_test[i]]+=1
-for i in range(4):
-    if app_count[i] != 0:
-        print(i, 'accuracy',suc_rate[i],'/',app_count[i]," ",suc_rate[i]/app_count[i])
-print('accuracy: ', acc / 54.0)
 
+# 학습
+log = LogisticRegression(x_train, y_train, x_test, y_test)
+bias, costs = log.learn(0.0000001, 1000)
+pre_cnt = 0
+pre_wr =0
+for i in range(0, 376):
+    prediction = log.predict(x_test[i], bias)
+    s1 = int(prediction/4)
+    s2 = prediction%4
+    reals1 = y_copy[i][0]
+    reals2 = y_copy[i][1]
+    if s1>s2 and reals1>reals2:
+        pre_wr +=1
+    elif s1==s2 and reals1==reals2:
+        pre_wr +=1
+    elif s1<s2 and reals1<reals2:
+        pre_wr +=1
+    print(i, ' predict: [',s1,' : ',s2,']', ' real: [',reals1,':',reals2,']')
+    if s1 == reals1 and s2 == reals2:
+        pre_cnt+=1
+print("accuracy = ",pre_cnt/376)
+print("winrate accuracy = ",pre_wr/376)
